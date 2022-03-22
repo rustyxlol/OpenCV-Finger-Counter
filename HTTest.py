becd4ad2ed3ccad52d1b36a46973ffc11af88c61
Rusty <imkhazix@gmail.com>
2022-03-22 13:52:57 +0530

    Setup: Deploying to Heroku

--- HTTest.py
+++ HTTest.py
@@ -1,40 +1,43 @@
 import cv2
 import time
 import HandTrackerModule as htm
+import streamlit as st
+
+
+st.title("Finger Counter")
+
+
+FRAME_WINDOW = st.image([])
+
 
 cap = cv2.VideoCapture(1)
 detector = htm.handDetector()
-previousTime = 0
-currentTime = 0
 
 tips = [4, 8, 12, 16, 20]
 while True:
-    success, img = cap.read()
-    img = detector.findHands(img)
-    landmarkList = detector.findPosition(img, draw=False)
-    if len(landmarkList) != 0:
-        fingers = []
+  success, img = cap.read()
+  img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-        # Thumb
-        if landmarkList[tips[0]][1] > landmarkList[tips[0] - 1][1]:
-            fingers.append(1)
-        else:
-            fingers.append(0)
+  FRAME_WINDOW.image(img)
+  img = detector.findHands(img)
+  landmarkList = detector.findPosition(img, draw=False)
+  if len(landmarkList) != 0:
+    fingers = []
 
-        # Fingers
-        for id in range(1,5):
-            if landmarkList[tips[id]][2] < landmarkList[tips[id]-2][2]:
-                fingers.append(1)
-            else:
-                fingers.append(0)
+    # Thumb
+    if landmarkList[tips[0]][1] > landmarkList[tips[0] - 1][1]:
+      fingers.append(1)
+    else:
+      fingers.append(0)
 
-        count = fingers.count(1)
-        cv2.putText(img, str(count), (50,200),cv2.FONT_HERSHEY_PLAIN,
-                    3, (255,0,255),3)
-    currentTime = time.time()
-    fps = 1 / (currentTime - previousTime)
-    previousTime = currentTime
-    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN,
-                3, (255, 0, 0), 3)
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    # Fingers
+    for id in range(1, 5):
+      if landmarkList[tips[id]][2] < landmarkList[tips[id]-2][2]:
+        fingers.append(1)
+      else:
+        fingers.append(0)
+
+    count = fingers.count(1)
+    cv2.putText(img, str(count), (50, 200), cv2.FONT_HERSHEY_PLAIN,
+                3, (255, 0, 255), 3)
+  FRAME_WINDOW.image(img)
--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # OpenCV Finger Counter
 
 Counts how many fingers are up<br>
-test
 ![Demo File](https://github.com/rustyxlol/OpenCV-Finger-Counter/blob/main/test.gif)
